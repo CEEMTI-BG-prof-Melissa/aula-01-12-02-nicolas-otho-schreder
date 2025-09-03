774ae91fd6037ce2901bfe3db32e8d422da5b1cf
Nicolas Otho Schreder <nicolas.oschreder@aluno.edu.es.gov.br>
2025-09-03 11:58:24 -0300

    Create index.php

---
+++ aula02-3tri/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 02 PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome1="Nicolas Otho Schrederr";
+
+            echo "Meu nome é: ".$nome1;
+
+        ?>
+    </body>
+</html>
