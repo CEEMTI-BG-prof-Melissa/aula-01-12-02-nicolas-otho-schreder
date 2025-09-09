00da38926b8b13b1d47e0449b982241504b57e53
Nicolas Otho Schreder <nicolas.oschreder@aluno.edu.es.gov.br>
2025-09-09 11:56:55 -0300

    Create index.php

---
+++ aula03-3tri/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title> Aula 03 PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome1="Nicolas Otho Schreder ";
+            $numero1=35;
+            $numero2=32;
+
+            $soma=$numero1+$numero2;
+
+            echo "Meu nome é: ".$nome1;
+            echo "<br> nome é $nome1 ";
+
+            echo "<br><br> A soma do $numero1 e $numero2 é igual: $soma"
+
+        ?>
+    </body>
+</html>
