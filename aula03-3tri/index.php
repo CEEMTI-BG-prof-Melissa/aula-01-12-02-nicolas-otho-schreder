fa4a62d35f31d69be3cabbfb0770647e0fcca859
Nicolas Otho Schreder <nicolas.oschreder@aluno.edu.es.gov.br>
2025-09-10 11:55:20 -0300

    Update index.php

--- aula03-3tri/index.php
+++ aula03-3tri/index.php
@@ -13,7 +13,11 @@
             echo "Meu nome é: ".$nome1;
             echo "<br> nome é $nome1 ";
 
-            echo "<br><br> A soma do $numero1 e $numero2 é igual: $soma"
+            echo "<br><br> A soma do $numero1 e $numero2 é igual a: $soma";
+            echo "<br> A subtraçao dos numeros $numero1 e $numero2 é igual a: " .($numero1-$numero2);
+            echo "<br> A multiplicaçao dos numeros $numero1 e $numero2 é igual a: " .($numero1*$numero2);
+            echo "<br> A multiplicaçao dos numeros $numero1 e $numero2 é igual a: " .($numero1/$numero2);
+            echo "<br> O resto da divisao dos numeros $numero1 e $numero2 é igual a: " .($numero1%$numero2);
 
         ?>
     </body>
